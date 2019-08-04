c6cc30c1c89e4cf57a983903ded5b82edee3d3fa
SychovSaveliy <sychov.saveliy@gmail.com>
2019-08-04 23:15:30 +0300

    small changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,10 @@
 <template>
-  <v-container>
-    Container
-    <v-btn>Click me</v-btn>
-    <div class="example">Example</div>
-  </v-container>
+  <v-app>
+    <v-app-bar app>
+      <v-container class="title">Car rental service</v-container>
+    </v-app-bar>
+    <v-content> </v-content>
+  </v-app>
 </template>
 
 <script>
